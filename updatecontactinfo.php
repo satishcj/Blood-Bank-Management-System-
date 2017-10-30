6af581ab752a6f467aafdfa8fd74c4e392ec8bd4
adarshpotharaju <32612201+adarshpotharaju@users.noreply.github.com>
2017-10-30 21:55:06 +0530

    Add files via upload

---
+++ updatecontactinfo.php
@@ -0,0 +1,24 @@
+﻿<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['alogin'])==0)
+	{	
+header('location:index.php');
+}
+else{
+// Code for change password	
+if(isset($_POST['submit']))
+{
+$address=$_POST['address'];
+$email=$_POST['email'];	
+$contactno=$_POST['contactno'];
+$sql="update tblcontactusinfo set Address=:address,EmailId=:email,ContactNo=:contactno";
+$query = $dbh->prepare($sql);
+$query->bindParam(':address',$address,PDO::PARAM_STR);
+$query->bindParam(':email',$email,PDO::PARAM_STR);
+$query->bindParam(':contactno',$contactno,PDO::PARAM_STR);
+$query->execute();
+$msg="Info Updateed successfully";
+}
+?>
