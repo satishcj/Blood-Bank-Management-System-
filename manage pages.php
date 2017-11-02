85764ac96823b362b9351f497f9453fd626e329d
adarshpotharaju <32612201+adarshpotharaju@users.noreply.github.com>
2017-11-02 14:12:14 +0530

    Add files via upload

---
+++ manage pages.php
@@ -0,0 +1,23 @@
+﻿<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['alogin'])==0)
+	{	
+header('location:index.php');
+}
+else{
+if($_POST['submit']=="Update")
+{
+	$pagetype=$_GET['type'];
+	$pagedetails=$_POST['pgedetails'];
+$sql = "UPDATE tblpages SET detail=:pagedetails WHERE type=:pagetype";
+$query = $dbh->prepare($sql);
+$query -> bindParam(':pagetype',$pagetype, PDO::PARAM_STR);
+$query-> bindParam(':pagedetails',$pagedetails, PDO::PARAM_STR);
+$query -> execute();
+$msg="Page data updated  successfully";
+
+}
+
+php?>
